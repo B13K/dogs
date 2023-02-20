3fed62af30c56288df3a81188840d57533110374
Jonathan <jonathan_3_7@hotmail.com>
2023-02-19 23:23:30 -0500

    Se agrego la paginacion

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -4,23 +4,42 @@ import { useState } from "react"
 import { useEffect } from "react"
 import Card from "../Card/Card"
 import style from "./CardContainer.module.css"
+import Pagination from "../Pagination/Pagination"
+
+
 const CardContainer = () => {
 
     const dogsAll = useSelector(state => state.dogs) //Traemos los dogs desde el store
+
         
-    const [dogs, setDogs] = useState({
-        dogs:[],
-    })
+    const [dogs, setDogs] = useState([])
+    
+    
+    //Paginacion
+    const [ currentPage, setCurrentPage ] = useState(1) //Pagina actual
+    const [ dogsPerPage ] = useState(10) //Cuarbos dogs habra por pagina
+    // End pagination
 
     useEffect( () => {
-        setDogs({...dogs, dogs: dogsAll})
+        setDogs(dogsAll)
+        setCurrentPage(1) //Se posiciona en la pagina uno cada vez que realiza un filtro
     },[dogsAll])
     
+    //Pagination
+    const indexOfLastDogs = currentPage * dogsPerPage;
+    const indexOfFirstDogs = indexOfLastDogs - dogsPerPage
+    const currentDogs = dogs.slice(indexOfFirstDogs, indexOfLastDogs)
+
+    const paginate = pageNumber => setCurrentPage(pageNumber)
+    //End Pagination
 
     return (
         <div className={style.container}>
+            <div>
+                <Pagination dogsPerPage={dogsPerPage} totalDogs = {dogs.length} paginate={paginate}/>
+            </div>
             {
-                dogs.dogs.map(e => <Card 
+                currentDogs.map(e => <Card 
                                 key={e.id}
                                 id = {e.id}
                                 name = {e.name}
---
+++ client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,29 @@
+
+
+const Pagination = ({dogsPerPage, totalDogs, paginate}) => {
+
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalDogs / dogsPerPage); i++) {
+        pageNumbers.push(i)
+        
+    }
+
+    return (
+        <nav>
+            <ul>
+                {
+                    pageNumbers.map( number => (
+                        <li key={number}>
+                            <button onClick={() => paginate(number)}>
+                                {number}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
